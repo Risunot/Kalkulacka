18f652ec213449dee3efdb49cf419612a7c05492
Ivana Hladová <ivuska169@gmail.com>
2022-08-22 14:51:38 +0200

    refactor:change functions to methods

--- app.js
+++ app.js
@@ -1,26 +1,3 @@
-
-
-// function reset() {
-//     document.getElementById("display_value").innerHTML = "";
-// }
-
-// function addValue(value) {
-//    let lastCharacter = document.getElementById("display_value").innerHTML;
-//    if (lastCharacter.slice(-1) == "+" && (value == "+" || value == "-" || value == "*" || value == "/")
-//         || lastCharacter.slice(-1) == "-" && (value == "-" || value == "+" || value == "*" || value == "/")
-//         || lastCharacter.slice(-1) == "*" && (value == "*" || value == "+" || value == "-" || value == "/")
-//         || lastCharacter.slice(-1) == "/" && (value == "/" || value == "+" || value == "-" || value == "*")) {
-//     return false;
-//    }
-   
-// function equals() {
-//     let result = document.getElementById("display_value").innerHTML;
-    
-//     document.getElementById("display_value").innerHTML  = eval(result);
-// }
-
-
-
 const calculator = {
     reset: function() {
         document.getElementById("display_value").innerHTML = "";
