d9393ac724da68b13a1a97eb844b32f3d709d7ad
Ivana Hladová <ivuska169@gmail.com>
2022-08-22 18:55:19 +0200

    refactor: change access to functions via modul pattern + add: gitignore

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 
-
-function reset() {
-    document.getElementById("display_value").innerHTML = "";
-}
-
-function addValue(value) {
-   let lastCharacter = document.getElementById("display_value").innerHTML;
-   if (lastCharacter.slice(-1) == "+" && (value == "+" || value == "-" || value == "*" || value == "/")
-        || lastCharacter.slice(-1) == "-" && (value == "-" || value == "+" || value == "*" || value == "/")
-        || lastCharacter.slice(-1) == "*" && (value == "*" || value == "+" || value == "-" || value == "/")
-        || lastCharacter.slice(-1) == "/" && (value == "/" || value == "+" || value == "-" || value == "*")) {
-    return false;
-   }
-   
-   document.getElementById("display_value").innerHTML += value;
-   console.log(typeof value)
-}
-
-function equals() {
-    let result = document.getElementById("display_value").innerHTML;
+const Calculator = (function () {
+    const reset = function() {
+        document.getElementById("display_value").innerHTML = "";
+    }
     
-    document.getElementById("display_value").innerHTML  = eval(result);
-}
+    const addValue = function(value) {
+       let lastCharacter = document.getElementById("display_value").innerHTML;
+       if (lastCharacter.slice(-1) == "+" && (value == "+" || value == "-" || value == "*" || value == "/")
+            || lastCharacter.slice(-1) == "-" && (value == "-" || value == "+" || value == "*" || value == "/")
+            || lastCharacter.slice(-1) == "*" && (value == "*" || value == "+" || value == "-" || value == "/")
+            || lastCharacter.slice(-1) == "/" && (value == "/" || value == "+" || value == "-" || value == "*")) {
+        return false;
+       }
+       
+       document.getElementById("display_value").innerHTML += value;
+       console.log(typeof value)
+    }
+    
+    const equals = function() {
+        let result = document.getElementById("display_value").innerHTML;
+        
+        document.getElementById("display_value").innerHTML  = eval(result);
+    }
+
+    return {
+        reset: reset,
+        addValue: addValue,
+        equals: equals
+    }
+}())
+
+
