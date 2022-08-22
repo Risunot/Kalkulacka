8124e8614f225723b4e9293b2e13dc9d325c9ebb
Ivana Hladová <ivuska169@gmail.com>
2022-08-22 14:45:47 +0200

    refactor:change functions to methods

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
 
 
-function reset() {
-    document.getElementById("display_value").innerHTML = "";
-}
+// function reset() {
+//     document.getElementById("display_value").innerHTML = "";
+// }
 
-function addValue(value) {
-   let lastCharacter = document.getElementById("display_value").innerHTML;
-   if (lastCharacter.slice(-1) == "+" && (value == "+" || value == "-" || value == "*" || value == "/")
-        || lastCharacter.slice(-1) == "-" && (value == "-" || value == "+" || value == "*" || value == "/")
-        || lastCharacter.slice(-1) == "*" && (value == "*" || value == "+" || value == "-" || value == "/")
-        || lastCharacter.slice(-1) == "/" && (value == "/" || value == "+" || value == "-" || value == "*")) {
-    return false;
-   }
+// function addValue(value) {
+//    let lastCharacter = document.getElementById("display_value").innerHTML;
+//    if (lastCharacter.slice(-1) == "+" && (value == "+" || value == "-" || value == "*" || value == "/")
+//         || lastCharacter.slice(-1) == "-" && (value == "-" || value == "+" || value == "*" || value == "/")
+//         || lastCharacter.slice(-1) == "*" && (value == "*" || value == "+" || value == "-" || value == "/")
+//         || lastCharacter.slice(-1) == "/" && (value == "/" || value == "+" || value == "-" || value == "*")) {
+//     return false;
+//    }
    
-   document.getElementById("display_value").innerHTML += value;
-   console.log(typeof value)
-}
-
-function equals() {
-    let result = document.getElementById("display_value").innerHTML;
+// function equals() {
+//     let result = document.getElementById("display_value").innerHTML;
     
-    document.getElementById("display_value").innerHTML  = eval(result);
-}
+//     document.getElementById("display_value").innerHTML  = eval(result);
+// }
+
+
+
+const calculator = {
+    reset: function() {
+        document.getElementById("display_value").innerHTML = "";
+    },
+    addValue: function(value) {
+        let lastCharacter = document.getElementById("display_value").innerHTML;
+        if (lastCharacter.slice(-1) == "+" && (value == "+" || value == "-" || value == "*" || value == "/")
+             || lastCharacter.slice(-1) == "-" && (value == "-" || value == "+" || value == "*" || value == "/")
+             || lastCharacter.slice(-1) == "*" && (value == "*" || value == "+" || value == "-" || value == "/")
+             || lastCharacter.slice(-1) == "/" && (value == "/" || value == "+" || value == "-" || value == "*")) {
+             return false;
+             }
+             document.getElementById("display_value").innerHTML += value;
+             console.log(typeof value);
+    },
+    equals: function() {
+        let result = document.getElementById("display_value").innerHTML;
+        
+        document.getElementById("display_value").innerHTML  = eval(result);
+    }
+}; 
+      
