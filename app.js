7c99ab8b09ddc5831e236c0863117f037ed7eeb7
Ivana Hladová <ivuska169@gmail.com>
2022-08-23 15:14:03 +0200

    feat: preventing repeated clicking to symbols('%', '(', ')', '.')

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ const Calculator = (function () {
     
     const addValue = function(value) {
        let lastCharacter = document.getElementById("display_value").innerHTML;
-       if (lastCharacter.slice(-1) == "+" && (value == "+" || value == "-" || value == "*" || value == "/")
-            || lastCharacter.slice(-1) == "-" && (value == "-" || value == "+" || value == "*" || value == "/")
-            || lastCharacter.slice(-1) == "*" && (value == "*" || value == "+" || value == "-" || value == "/")
-            || lastCharacter.slice(-1) == "/" && (value == "/" || value == "+" || value == "-" || value == "*")) {
+       if (lastCharacter.slice(-1) == "+" && (value == "+" || value == "-" || value == "*" || value == "/" || value == "%" || value == "(" || value == ")" || value == ".")
+           || lastCharacter.slice(-1) == "-" && (value == "-" || value == "+" || value == "*" || value == "/" || value == "%" || value == "(" || value == ")" || value == ".")
+           || lastCharacter.slice(-1) == "*" && (value == "*" || value == "+" || value == "-" || value == "/" || value == "%" || value == "(" || value == ")" || value == ".")
+           || lastCharacter.slice(-1) == "/" && (value == "/" || value == "+" || value == "-" || value == "*" || value == "%" || value == "(" || value == ")" || value == ".")
+           || lastCharacter.slice(-1) == "%" && (value == "-" || value == "+" || value == "*" || value == "/" || value == "%" || value == "(" || value == ")" || value == ".") 
+           || lastCharacter.slice(-1) == "(" && (value == "-" || value == "+" || value == "*" || value == "/" || value == "%" || value == "(" || value == ")" || value == ".") 
+           || lastCharacter.slice(-1) == ")" && (value == "-" || value == "+" || value == "*" || value == "/" || value == "%" || value == "(" || value == ")" || value == ".")
+           || lastCharacter.slice(-1) == "." && (value == "-" || value == "+" || value == "*" || value == "/" || value == "%" || value == "(" || value == ")" || value == ".")) {
         return false;
        }
        
